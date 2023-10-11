76ab97fe5d204c1f3293ae353c138a653193a0b0
zdkHenrique <125223003+zdkHenrique@users.noreply.github.com>
2023-10-11 10:18:46 -0300

    Add files via upload

---
+++ PWNB_ENTREGAS/PWNB-Semana#09/dom.js
@@ -0,0 +1,10 @@
+function redirectPage() {
+    window.alert("Login Concluido!");
+    window.location.href = 'home.html';
+}
+
+function clicklogin() {
+    
+    document.getElementById('logout').innerHTML = "Log-out";
+
+}
